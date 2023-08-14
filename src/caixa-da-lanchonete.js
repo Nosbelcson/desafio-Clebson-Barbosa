c86e5cc38bc7bbc3dff80550ef5964cc96f88f8c
Clebson Barbosa <119675718+Nosbelcson@users.noreply.github.com>
2023-08-14 18:46:37 -0300

    versão final do teste
    versão final com todos os teste positivos

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,52 +1,67 @@
 class CaixaDaLanchonete {
-    static calcularValorDaCompra(pedido, formaDePagamento) {
-      const cardapio = {
-        cafe: { descricao: 'Café', valor: 3.00 },
-        chantily: { descricao: 'Chantily (extra do Café)', valor: 1.50 },
-        suco: { descricao: 'Suco Natural', valor: 6.20 },
-        sanduiche: { descricao: 'Sanduíche', valor: 6.50 },
-        queijo: { descricao: 'Queijo (extra do Sanduíche)', valor: 2.00 },
-        salgado: { descricao: 'Salgado', valor: 7.25 },
-        combo1: { descricao: '1 Suco e 1 Sanduíche', valor: 9.50 },
-        combo2: { descricao: '1 Café e 1 Sanduíche', valor: 7.50 },
-      };
-  
-      const descontoDinheiro = 0.05;
-      const acrescimoCredito = 0.03;
-  
-      if (!pedido.length) {
-        return 'Não há itens no carrinho de compra!';
-      }
-  
-      let total = 0;
-      let temItemPrincipal = false;
-  
-      for (const item of pedido) {
-        if (!cardapio[item.codigo]) {
-          return 'Item inválido!';
-        }
-  
-        if (item.codigo !== 'chantily' && item.codigo !== 'queijo') {
-          temItemPrincipal = true;
-        }
-  
-        total += cardapio[item.codigo].valor * item.quantidade;
-      }
-  
-      if (!temItemPrincipal) {
-        return 'Item extra não pode ser pedido sem o principal';
-      }
-  
-      if (formaDePagamento === 'dinheiro') {
-        total = total - (total*descontoDinheiro);
-      } else if (formaDePagamento === 'credito') {
-        total= total + (total*acrescimoCredito);
-      } else if (formaDePagamento !== 'debito') {
-        return 'Forma de pagamento inválida!';
-      }
-  
-      return total.toFixed(2);
+  calcularValorDaCompra(formaDePagamento, pedido) {
+    const cardapio = {
+      cafe: 3.00,
+      chantily: 1.50,
+      suco: 6.20 ,
+      sanduiche: 6.50 ,
+      queijo: 2.00 ,
+      salgado: 7.25,
+      combo1: 9.50 ,
+      combo2: 7.50 ,
+    };
+
+    if(!(formaDePagamento === 'dinheiro' || formaDePagamento === 'debito' || formaDePagamento === 'credito')){
+      return 'Forma de pagamento inválida!'
     }
+
+    const descontoDinheiro = 0.05;
+    const acrescimoCredito = 0.03;
+
+    if (pedido.length===0) {
+      return 'Não há itens no carrinho de compra!';
+    }
+
+    let total = 0;
+    let temItemPrincipalCafe = false;
+    let temItemPrincipalSanduiche = false;
+
+    for (let i = 0; i < pedido.length; i++) {
+      let items = pedido[i]
+      let item = items.split(',')
+
+      if (!cardapio[item[0]]) {
+        return 'Item inválido!'
+      }
+
+      if (parseInt(item[1]) <= 0){
+        return 'Quantidade inválida!'
+      }
+
+      if (item[0] === 'cafe' || item[0] === 'sanduiche') {
+        if (item[0] === 'cafe') {
+          temItemPrincipalCafe = true
+        }
+        if (item[0] === 'sanduiche') {
+          temItemPrincipalSanduiche = true
+        }
+      } else if (item[0] === 'chantily' && !temItemPrincipalCafe) {
+        return 'Item extra não pode ser pedido sem o principal'
+      } else if (item[0] === 'queijo' && !temItemPrincipalSanduiche) {
+        return 'Item extra não pode ser pedido sem o principal'
+      }
+
+      total += cardapio[item[0]] * parseInt(item[1])
+    }
+
+    if (formaDePagamento === 'dinheiro') {
+      total = total - (total * descontoDinheiro)
+    } else if (formaDePagamento === 'credito') {
+      total = total + (total * acrescimoCredito)
+    }
+
+    return `R$ ${total.toFixed(2).replace('.',',')}`;
+  }
 }
-    
-    export { CaixaDaLanchonete };
+
+export { CaixaDaLanchonete };
